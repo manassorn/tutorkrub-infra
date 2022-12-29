0a8919cd9107d76dec358643c65a6d1e74745f5e
mvanichdilok <manassorn.vanichdilokkul@agoda.com>
2022-12-29 15:29:34 +0700

    first commit

---
+++ install-docker.sh
@@ -0,0 +1,13 @@
+#install docker
+sudo yum install -y yum-utils
+sudo yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo docker run hello-world
+
+#install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
---
+++ monitoring/startup.sh
@@ -0,0 +1,4 @@
+# TODO: check if docker's already started
+sudo service docker start
+
+docker-compose up -d
